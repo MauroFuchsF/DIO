3e1cef1df3f462c50a7d6a134cc81b81059037ef
MauroFuchsF <maurofuchsf@hotmail.com>
2023-12-06 20:20:21 -0300

    projeto pokedex

---
+++ Aulas JS/Aula03 Funçoes/aula03.js
@@ -0,0 +1,15 @@
+function escrevaMeuNome(nome) {
+    console.log('Meu nome é ' + nome);
+}
+escrevaMeuNome('mauro');
+
+function verificarIdade(idade){
+    if (idade >=18){
+        console.log('Maior');
+    } else {
+        console.log('Menor');
+    }
+}
+
+
+verificarIdade(30);
---
+++ Aulas JS/aula 04 Objetos/aula04.js
@@ -0,0 +1,8 @@
+const mauro = {
+    nome: 'Mauro',
+    idade: 30
+};
+
+mauro.altura = 1.74;
+
+console.log(mauro);
